22e6872b83c8cea75cbabbf8c3dfe4e4892b6210
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-07-13 17:10:27 +0200

    finished second task of homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import { Profile } from './Profile';
 import user from '../data/user';
+import { Statistics } from './Statistics';
+import data from '../data/data';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="UPLOAD STATS" stats={data} />
+    </div>
   );
 };
